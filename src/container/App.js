7c74dfc4d1f33cdecce29431b04d0d73aef96153
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-19 12:12:19 +0100

    Changed some of the state variables to be constants

--- src/container/App.js
+++ src/container/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 
+import classes from './App.module.css';
 import Temperature from '../components/Temperature/Temperature';
 import Controller from '../components/Controller/Controller';
 import PowerSavingMode from '../components/PowerSavingMode/PowerSavingMode';
 import WeatherCity from '../components/WeatherCity/WeatherCity'
-import classes from './App.module.css';
+
+const DEFAULT_TEMPERATURE = 20;
+const MINIMUM_TEMPERATURE = 10;
+const MAXIMUM_TEMPERATURE_PSM_ON = 25;
+const MAXIMUM_TEMPERATURE_PSM_OFF = 32;
+const MEDIUM_ENERGY_USAGE_LIMIT = 18;
 
 class App extends Component {
   state = {
     temperature: 20,
     powerSavingMode: true,
-    defaultTemperature: 20,
-    minimumTemperature: 10,
-    maximumTemperaturePSMOn: 25,
-    maximumTemperaturePSMOff: 32,
-    mediumEnergyUsageLimit: 18,
     city: 'london'
   }
 
   up = () => {
     const currentTemperature = this.state.temperature
-    if (this.state.powerSavingMode && currentTemperature < this.state.maximumTemperaturePSMOn) {
+    if (this.state.powerSavingMode && currentTemperature < MAXIMUM_TEMPERATURE_PSM_ON) {
       this.setState({ temperature: currentTemperature + 1 })
     }
-    if (!this.state.powerSavingMode && currentTemperature < this.state.maximumTemperaturePSMOff) {
+    if (!this.state.powerSavingMode && currentTemperature <  MAXIMUM_TEMPERATURE_PSM_OFF) {
       this.setState({ temperature: currentTemperature + 1 })
     }
   }
@@ -34,22 +35,22 @@ class App extends Component {
   }
 
   reset = () => {
-    this.setState({ temperature: this.state.defaultTemperature })
+    this.setState({ temperature: DEFAULT_TEMPERATURE })
   }
 
   togglePowerSavingModeHandler = () => {
     const powerSavingModeSwitch = this.state.powerSavingMode;
     this.setState({ powerSavingMode: !powerSavingModeSwitch })
-    if (!powerSavingModeSwitch && this.state.temperature > this.state.maximumTemperaturePSMOn) {
-      this.setState({ temperature: this.state.maximumTemperaturePSMOn })
+    if (!powerSavingModeSwitch && this.state.temperature > MAXIMUM_TEMPERATURE_PSM_ON) {
+      this.setState({ temperature: MAXIMUM_TEMPERATURE_PSM_ON })
     }
   }
 
   energyUsage = () => {
-    if (this.state.temperature < this.state.mediumEnergyUsageLimit) {
+    if (this.state.temperature < MEDIUM_ENERGY_USAGE_LIMIT) {
       return classes.LowUsage;
     }
-    if (this.state.temperature >= this.state.mediumEnergyUsageLimit && this.state.temperature <= this.state.maximumTemperaturePSMOn) {
+    if (this.state.temperature >= MEDIUM_ENERGY_USAGE_LIMIT && this.state.temperature <= MAXIMUM_TEMPERATURE_PSM_ON) {
       return classes.MediumUsage;
     }
     return classes.HighUsage;
