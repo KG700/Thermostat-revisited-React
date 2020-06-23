463443f65f8d4ce8ae7449a62c8b892a4900d321
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-23 10:02:27 +0100

    Added an /all endpoint to retrieve all values of thermostat

--- app.rb
+++ app.rb
@@ -4,7 +4,18 @@ require_relative './lib/thermostat'
 
 class ThermostatApp < Sinatra::Base
 
-  
+  get "/" do
+    File.read('public/index.html')
+  end
+
+  get "/all" do
+    thermostat = Thermostat.instance
+    {
+      temperature: thermostat.temperature,
+      powerSavingMode: thermostat.power_saving_mode,
+      city: thermostat.city
+    }.to_json
+  end
 
   run! if app_file == $0
 end
